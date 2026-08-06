81d541839e572474d38c6f687b4b24662ec3db7c
Yuki Little <nevan.little@hotmail.com>
2026-08-06 14:06:57 -0600

    Developing the infrastructure for obstacles
    static obstacles should be functional, the moving ones are still being developed

---
+++ Assets/_Project/Code/Gameplay/Obstacles/ObstacleInfo.cs
@@ -0,0 +1,40 @@
+﻿using Assets._Project.Code.Gameplay.Obstacles.Stratagies;
+using UnityEngine;
+
+namespace Assets._Project.Code.Gameplay.Obstacles
+{
+    [CreateAssetMenu(fileName = "ObstacleInfo", menuName = "Scriptable Objects/ObstacleInfo")]
+    public class ObstacleInfo : ScriptableObject
+    {
+        public float Damage;
+        public float KnockbackForce;
+        public float StunTime;
+        public string PlayerLayerName;
+    }
+
+    #region Moving Obstacle Info Set
+    [CreateAssetMenu(fileName = "BiteInfo", menuName = "Scriptable Objects/BiteInfo")]
+    public class  BiteInfo : ScriptableObject
+    {
+        public Vector2 BiteOffset;
+        public Vector2 BiteHalfExtent;
+        public float BiteDuration;
+    }
+    [CreateAssetMenu(fileName = "SearchInfo", menuName = "Scriptable Objects/SearchInfo")]
+    public class SearchInfo : ScriptableObject
+    {
+        public SearchStratagies SearchStratagy;
+        public float RaycastDistance;
+    }
+    [CreateAssetMenu(fileName = "IdleInfo", menuName = "Scriptable Objects/IdleInfo")]
+    public class IdleInfo : ScriptableObject
+    {
+
+    }
+    [CreateAssetMenu(fileName = "ChaseInfo", menuName = "Scriptable Objects/ChaseInfo")]
+    public class ChaseInfo : ScriptableObject
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/_Project/Code/Gameplay/Obstacles/StaticObstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace Assets._Project.Code.Gameplay.Obstacles
+{
+    public class StaticObstacle : BaseObstacle
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            _playerController = collision.GetComponent<PlayerController>();
+            if (_playerController != null)
+            {
+                HurtPlayer();
+            }
+        }
+    }
+}
